f3d0a15b0e08b6adc3eeff05c9feedfd8f62d765
KHALIFA Haithem <khalifa.haithem@gmail.com>
2025-01-13 22:28:14 +0100

    tmp

--- DotNetOtelExample/obj/Debug/net8.0/DotNetOtelExample.AssemblyInfo.cs
+++ DotNetOtelExample/obj/Debug/net8.0/DotNetOtelExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetOtelExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7d9ceeea337032352af421593fa66c51a5edf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetOtelExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetOtelExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- deploy.ps1
+++ deploy.ps1
@@ -20,13 +20,15 @@ kubectl apply -f grafana-deployment.yaml
 
 kubectl apply -f otel-collector-config.yaml
 kubectl apply -f otel-collector-deployment.yaml
+kubectl apply -f otel-collector-service.yaml
 
 kubectl apply -f loki-deployment.yaml
 
 kubectl apply -f tempo-config.yaml
 kubectl apply -f tempo-deployment.yaml
 
+kubectl port-forward service/dotnet-webapi 8080:8080
+
 minikube service grafana
 
-minikube service dotnet-webapi
 # registry   ClusterIP   10.109.84.137   <none>        80/TCP,443/TCP   20s
